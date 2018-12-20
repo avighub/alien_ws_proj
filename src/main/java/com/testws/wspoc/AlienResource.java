17a86220f350312de360372163366454f430ad24
Avishek <Avishek@192.168.56.1>
2018-12-20 18:18:19 +0530

    Project Renamed from wsproj to alien_ws_proj

--- src/main/java/com/testws/wspoc/AlienResource.java
+++ src/main/java/com/testws/wspoc/AlienResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -15,11 +16,21 @@ public class AlienResource {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public List<Alien> getAlien(){
+	public List<Alien> getAliens(){
 		System.out.println("Calling GET -aliens");
 		
 		
 		return repo.getAliens();
+	}
+	
+	@GET
+	@Path("alien/{id}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Alien getAlien(@PathParam("id") int id){
+		System.out.println("Calling GET -alien");
+		
+		
+		return repo.getAlien(id);
 	}
 	
 	@POST
